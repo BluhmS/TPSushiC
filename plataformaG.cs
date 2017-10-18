78ee7ad7fe623f95457c0afb65854c82bdbafee8
San7e <sante.bluhm@gmail.com>
2017-10-17 22:56:08 -0300

    first commit

---
+++ plataformaG.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaG : MonoBehaviour {
+
+    public int puntaje = 0;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider == "Circu")
+        {
+            puntaje++;
+        }
+            
+        print("Has ganado " +puntaje +"puntos!");
+    }
+}
